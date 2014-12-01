82ad9093be478f71ec056a874045495fad3f62c4
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2014-12-01 17:19:58 +0100

    Update coduno.rb

--- coduno.rb
+++ coduno.rb
@@ -3,6 +3,8 @@ require 'formula'
 class Coduno < Formula
   homepage 'http://cod.uno'
   url 'https://drone.io/github.com/coduno/cli/files/darwin-amd64/coduno'
+  version 'latest'
+
   def install
     bin.install 'coduno'
   end
